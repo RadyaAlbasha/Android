fa3679ba626e068751dd7665bf37aed6a139b68d
RadyaAlbasha <47277078+RadyaAlbasha@users.noreply.github.com>
2019-04-05 15:01:52 +0200

    update
    Now ِApp can receive images from other Apps

--- LabsDay8/DownloadImages/app/src/main/java/android/day8/lab1/downloadimages/ShowImageActivity.java
+++ LabsDay8/DownloadImages/app/src/main/java/android/day8/lab1/downloadimages/ShowImageActivity.java
@@ -1,9 +1,13 @@
 package android.day8.lab1.downloadimages;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.io.DataInputStream;
@@ -16,16 +20,20 @@ import java.io.IOException;
 public class ShowImageActivity extends AppCompatActivity {
 
     ImageView imgView;
+    Bitmap imgBitmap;
+    String fileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_image);
         imgView = findViewById(R.id.imageView);
-        String fileName = getIntent().getStringExtra("fileName");
+        Intent intent = getIntent();
+        fileName = intent.getStringExtra("fileName");
 
-        Bitmap imgBitmap =loadImageFromStorage(fileName);
+        imgBitmap =loadImageFromStorage(fileName);
         //imgView.setImageBitmap(Bitmap.createScaledBitmap(imgBitmap, imgView.getWidth(),imgView.getHeight(), false));
         imgView.setImageBitmap(imgBitmap);
+        receivImage(intent);
     }
 
    /* public byte[] loadInternalStorage(String _fileName)
@@ -62,5 +70,30 @@ public class ShowImageActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return bitmap;
+    }
+    //receiv image from another app
+    private void receivImage(Intent intent)
+    {
+        String action = intent.getAction();
+        String type = intent.getType();
+        if(Intent.ACTION_SEND.equals(action) && type!=null)
+        {
+            if(type.startsWith("image/")){
+                Uri imgUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if(imgUri!=null){
+                    ImageView imgView = findViewById(R.id.imageView);
+                    imgView.setImageURI(imgUri);
+                    Log.i("testing incoming image ","oncreate ShowImageActivity : "+imgUri);
+                }
+            }
+        }
+    }
+
+    public void shareImage(View view) {
+        final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("image/jpeg");
+        final File photoFile = new File(fileName, "profile.jpg");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(photoFile));
+        startActivity(Intent.createChooser(shareIntent, "Share image using"));
     }
 }
